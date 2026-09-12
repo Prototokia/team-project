7e818861cc63bdc2be366eed9593645b1d5fae77
Sergey Filippov <filippovserg07@mail.ru>
2026-09-12 17:30:14 +0700

    Исправлены замечания ревью

--- filippov.cpp
+++ filippov.cpp
@@ -1,12 +1,11 @@
-#include "filippov.h"
-
+const double KG_TO_LB_FACTOR = 2.20462;
 // ===== Расчетные функции варианта 66 =====
 // Конвертер величин: килограммы -> фунты
 double kgToLb(double kg) {
-	return kg * 2.20462;
+	return kg * KG_TO_LB;
 }
 
 // Конвертер величин: фунты -> килограммы
 double lbToKg(double lb) {
-	return lb / 2.20462;
+	return lb / KG_TO_LB;
 }
--- filippov.h
+++ filippov.h
@@ -1,8 +1,4 @@
-#ifndef FILIPPOV_H
-#define FILIPPOV_H
-
+#pragma once
 // функции варианта 66 (Филиппов С. Н.)
 double kgToLb(double kg);
 double lbToKg(double lb);
-
-#endif // FILIPPOV_H
