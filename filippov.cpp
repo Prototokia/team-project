6f5b29bceb58a20398d55835866d72094b954120
Sergey Filippov <filippovserg07@mail.ru>
2026-09-12 17:41:45 +0700

    Добавлена правильная константа для расчетов

--- filippov.cpp
+++ filippov.cpp
@@ -1,4 +1,4 @@
-const double KG_TO_LB_FACTOR = 2.20462;
+const double KG_TO_LB = 2.20462;
 // ===== Расчетные функции варианта 66 =====
 // Конвертер величин: килограммы -> фунты
 double kgToLb(double kg) {
