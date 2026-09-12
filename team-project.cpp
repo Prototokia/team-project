82baed578510ffde4b10607755304b6ebe575bb1
Artem Vazhev <vazhev_artem@mail.ru>
2026-09-12 14:36:13 +0700

    Добавлены пункты меню варианта 71

--- team-project.cpp
+++ team-project.cpp
@@ -4,18 +4,39 @@
 #include "team-project.h"
 #include "vazhev.h"
 // #include "petrov.h"
-// #include "sidorova.h"
 // === КОНЕЦ БЛОКА ПОДКЛЮЧЕНИЙ ===
 int main() {
 	int choice;
+	int sec;
 	do {
 		std::cout << "\n=== Командный проект: сборник расчётов ===\n";
 		// === БЛОК МЕНЮ: каждый участник добавляет свои пункты ===
 		// === КОНЕЦ БЛОКА МЕНЮ ===
 		std::cout << "0. Выход\n";
+		std::cout << "1. Секунды => часы\n";
+		std::cout << "2. Секунды => минуты\n";
+		std::cout << "3. Секунды => секунды\n";
 		std::cout << "Выберите пункт: ";
 		std::cin >> choice;
 		switch (choice) {
+		case 1: {
+			std::cout << "Введите количество секунд: ";
+			std::cin >> sec;
+			std::cout << "В " << sec << " секундах " << hoursOf(sec) << " часов.\n";
+			break;
+		}
+		case 2: {
+			std::cout << "Введите количество секунд: ";
+			std::cin >> sec;
+			std::cout << "В " << sec << " секундах " << minutesOf(sec) << " минут.\n";
+			break;
+		}
+		case 3: {
+			std::cout << "Введите количество секунд: ";
+			std::cin >> sec;
+			std::cout << "В " << sec << " секундах " << minutesOf(sec) << " секунд.\n";
+			break;
+		}
 			
 		case 0:
 			std::cout << "Работа завершена.\n";
